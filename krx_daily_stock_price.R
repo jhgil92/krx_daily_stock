9a5f06ac9bd8464653a47ce0a319887a6d94c5ad
JIHONG GIL <42768552+jhgil92@users.noreply.github.com>
2019-08-05 09:35:45 +0900

    Update krx_daily_stock_price.R

--- krx_daily_stock_price.R
+++ krx_daily_stock_price.R
@@ -5,28 +5,45 @@ library(dplyr)
 library(httr)
 library(readr)
 
-date = "20190628"
+date = "20190627"
 
-POST(url = "http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx",
-     query = list(
-       name         = 'filedown',
-       filetype     = 'csv',
-       url          = 'MKD/04/0404/04040200/mkd04040200_01',
-       market_gubun = 'ALL',
-       indx_ind_cd  = "",
-       sect_tp_cd   = "",
-       schdate      = date,
-       pagePath     = "/contents/MKD/04/0404/04040200/MKD04040200.jsp"
-     )) -> otp
-otp = content(otp, "text")
+get_daily_stock <- function(date){
+  get_market <- function(date, market){
+    POST(url = "http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx",
+         query = list(
+           name         = 'filedown',
+           filetype     = 'csv',
+           url          = 'MKD/04/0404/04040200/mkd04040200_01',
+           market_gubun = market,
+           indx_ind_cd  = "",
+           sect_tp_cd   = "",
+           schdate      = date,
+           pagePath     = "/contents/MKD/04/0404/04040200/MKD04040200.jsp"
+         )) -> otp
+    otp = content(otp, "text")
+    
+    POST(url = "http://file.krx.co.kr/download.jspx",
+         query = list(
+           code = otp
+         ),
+         add_headers(referer = "http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx")
+    ) %>%
+      read_html %>%
+      html_text %>%
+      read_csv -> data1
+    data1 <- cbind(일자 = date, 구분 = market, data1)
+    data1 <- data1[-3]
+    Sys.sleep(5)
+    return(data1)
+  }
+  
+  STK <- get_market(date = date, market = "STK")
+  KSQ <- get_market(date = date, market = "KSQ")
+  KNX <- get_market(date = date, market = "KNX")
+  
+  data1 <- rbind(STK, KSQ, KNX)
+  
+  assign(paste0("stock_", date), data1, envir = .GlobalEnv)
+}
 
-
-POST(url = "http://file.krx.co.kr/download.jspx",
-     query = list(
-       code = otp
-     ),
-     add_headers(referer = "http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx")
-     ) %>%
-  read_html %>%
-  html_text %>%
-  read_csv -> data1
+get_daily_stock(date)
